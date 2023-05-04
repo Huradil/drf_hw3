65ea54b009e47b6967a6785790322865d0117716
Huradil <nuradiltolokov@gmail.com>
2023-05-04 14:02:53 +0200

    commit for aws

--- hospital/settings.py
+++ hospital/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
+    'drf_yasg',
 
     'app',
 ]
@@ -47,6 +49,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -120,8 +123,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL='media/'
+MEDIA_ROOT=BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
--- hospital/urls.py
+++ hospital/urls.py
@@ -16,8 +16,32 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Backend for Hospital",
+      default_version='alpha-0.0.1',
+      description="This is API for Hospital",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="nuradiltolokov@gmail.com"),
+      license=openapi.License(name="NO Licence"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+swagger_urlpatterns=[
+   path('swagger/',schema_view.with_ui('swagger',cache_timeout=0)),
+]
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('app.urls')),
-]
+]+swagger_urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
